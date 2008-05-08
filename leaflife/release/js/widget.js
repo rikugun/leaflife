9f2769c3d2e6c65050d61e89d96a6b931d725bef
chao.blueaqua <chao.blueaqua@43e53afd-884c-0410-9b85-8b5e0eee1c45>
2008-05-08 08:46:12 +0000

    WidgetLoader 1.0.3

---
+++ leaflife/release/js/widget.js
@@ -0,0 +1,20 @@
+/*
+    WidgetLoader (version 1.0.3)
+
+    Copyright 2008 Huangchao.
+
+    This file is part of LeafLife.
+
+    LeafLife is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License version 2 as
+    published by the Free Software Foundation.
+
+    LeafLife is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with LeafLife. If not, see <http://www.gnu.org/licenses/>.
+*/
+eval(function(B,D,A,G,E,F){function C(A){return A<62?String.fromCharCode(A+=A<26?65:A<52?71:-4):A<63?'_':A<64?'$':C(A>>6)+C(A&63)}while(A>0)E[C(G--)]=D[--A];return B.replace(/[\w\$]+/g,function(A){return E[A]==F[A]?A:E[A]})}('(7(){g S=8,O=S.expando?a:Z,T=S.getElementsByTagName("head")[Z],B=T.childNodes,H,Q,K,J={i:"2/i"+O,y:"2/y"+O,o:"o/o"+O,q:"q/q"+O},BK={i:"a.Z.17",y:"Z.BR",o:"a.b.27",q:"Z.a"},BE={o:["o/o","o/BL/5/5"],q:["q/q","q/BL/5/5"]},F={o:["i"],q:["i"]};h(H=B.u-a;H>=Z;--H){K=B[H].k;e(K&&(Q=K.search(/\\bwidget.BM\\U/))>-a){e(!S._)S._=B[H].BI("_")||"";g D=K.substring(Z,Q),E=B[H].BI("imports"),P,d,N,R,I,L,G,M,c,A;e(E){P=E.toLowerCase().split(","),d={},N=[];h(M=P.u-a;M>=Z;--M){R=P[M].3(/^\\W+/,"").3(/\\W+d/,"");I=R.3(/^[BA-Y]/,7(d){t d.BC()});e(!w[I]&&!d[R]){d[R]=a;N[N.u]=R,G=BE[R];e(L=F[R])h(c=L.u-a;c>=Z;--c){R=L[c];I=R.3(/^[BA-Y]/,7(d){t d.BC()});e(!d[R]&&!w[I]){N[N.u]=R;d[R]=a}}e(G)h(c=G.u-a;c>=Z;--c){A=S.BP("link");T.BF(A);A.href=D+G[c]+".css";A.rel="stylesheet"}}}h(M=N.u-a;M>=Z;--M){I=N[M];e(J[I]){A=S.BP("script");T.BF(A);A.k=D+J[I]+".BM?ver="+BK[I]}}}s}}e(!S.4){S.4=[];S.addWidget=7(c,A){g d=8.4;d[d.u]=c;e(A)d["c"+A]=c};S.getWidgetById=7(d){t 8.4["c"+d]};S.removeWidgetById=7(c){g d=8.4["c"+c];e(d)d.BJ()};g C=7(){g c=8.4,d=c.u-a;h(;d>=Z;--d)c[d].BJ();8.4=n};e(O){S.execCommand("BackgroundImageCache",BG,true);w.attachEvent("onunload",C)}r w.addEventListener("unload",C,BG);Function.9.hook=7(E,A,C,c,D,d,B){g F=m;t 7(){F.call(E,A,C,c,D,d,B)}};1.9.trim=7(){t m.3(/^\\W+/,"").3(/\\W+d/,"")};1.9.$=7(){g d=m;e(/["\\\\\\l-\\j]/.test(d))d=d.3(/[\\l-\\j\\\\"]/BQ,7(c){g d={"\\U":"\\\\U","\\X":"\\\\X","\\BO":"\\\\BO","\\BD":"\\\\BD","\\V":"\\\\V","\\"":"\\\\\\"","\\\\":"\\\\\\\\"},A=d[c];e(A)t A;A=c.charCodeAt();t"\\\\u00"+Math.floor(A/f).BH(f)+(A%f).BH(f)});t"\\""+d+"\\""};Boolean.9.$=7(){t""+m};Date.9.$=7(){t"@"+m.getTime()};Number.9.$=7(){t isFinite(m)?""+m:n};Array.9.$=7(d){e(!d)d=Z;r e(d>b)t n;++d;g B=[],A=Z,c;h(;A<m.u;++A){c=m[A];0(v c){p"z":e(c==n||v c.$!=="7")s;p"x":p"BB":p"6":c=c.$(d);e(c!=n)B[B.u]=c}}t"["+B.BN(",")+"]"};Object.9.$=7(d){e(!d)d=Z;r e(d>b)t n;++d;g B=[],A,c,C;h(A in m)e(v A==="x"){c=m[A];C=n;0(v c){p"z":e(c==n){C="n";s}r e(v c.$!=="7")s;p"6":p"BB":p"x":C=c.$(d);s;p"undefined":C="n"}e(C!=n)B[B.u]=A.$()+":"+C}t"{"+B.BN(",")+"}"}}})()','O|P|Q|R|S|U|V|b|r|s|t|z|0|1|2|_|$|if|16|var|for|map|x1f|src|x00|this|null|tree|case|grid|else|break|return|length|typeof|window|string|remote|object|switch|String|common|replace|widgets|default|boolean|function|document|prototype|contextPath|toJSONString|a|number|toUpperCase|f|T|appendChild|false|toString|getAttribute|destructor|N|themes|js|join|n|createElement|g|4'.split('|'),69,81,{},{}))
